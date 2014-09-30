7080e3a91980124993798db0593924ea3d15b018
Matt Cox <matt.cox11@gmail.com>
2014-09-30 15:23:59 -0400

    Initial Commit

---
+++ AgeOfLaika/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AgeOfLaika
+//
+//  Created by Matthew Cox on 9/30/14.
+//  Copyright (c) 2014 Matthew Cox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
